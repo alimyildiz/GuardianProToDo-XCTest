b3f6ec5293d8d928ff5806dd4e8d67a4bced6098
alimyildiz <alim.yldz.cmpe@gmail.com>
2022-04-27 03:24:21 +0300

    added component

---
+++ GuardianProToDo/Components/DefaultCurrencyComponent.swift
@@ -0,0 +1,44 @@
+//
+//  DefaultCurrencyComponent.swift
+//  GuardianProToDo
+//
+//  Created by Alim Yıldız on 4/27/22.
+//
+
+import UIKit
+
+struct DefaultCurrencyComponentModel {
+    
+    var defaultCurrencyName: String?
+    var controller: UIViewController?
+    
+    init(defaultCurrencyName: String? = nil ,controller: UIViewController? = nil){
+        self.defaultCurrencyName = defaultCurrencyName
+        self.controller = controller
+    }
+}
+
+class DefaultCurrencyComponent: BBaseTableViewCell {
+
+    @IBOutlet weak var defaultCurrencyLabel: UILabel!
+    @IBOutlet weak var defaultAmountLabel: UILabel!
+    override public func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override public func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    override public func createViewObject(baseObject: Any!, indexPath: IndexPath) {
+        super.createViewObject(baseObject: baseObject, indexPath: indexPath)
+        
+        if let model = self.baseObject.viewData as? DefaultCurrencyComponentModel {
+            self.defaultAmountLabel.text = "1"
+            self.defaultCurrencyLabel.text = model.defaultCurrencyName
+        }
+    }
+    
+}
